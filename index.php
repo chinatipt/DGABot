49acd7daf3a77739071f8b95d6fe1512046154c7
chinatipt <51694748+chinatipt@users.noreply.github.com>
2019-07-14 16:42:25 +0700

    Update index.php

--- index.php
+++ index.php
@@ -16,8 +16,8 @@
  */
 require_once('./LINEBotTiny.php');
 
-$channelAccessToken = '+KBu0BMXcqYIBvbzJh+ocF7BoG20n8CtxE9Hw0ceHFvQVUA3oaCZl63FyhTlvcMInaL4Wrp99/Mmszf+rCdPctkkc8ya3QLcA8hftSstBfIlbEWrrz8dPM3Dh6thJ9H5UkhO+FsnPPNvcwFFnTGibwdB04t89/1O/w1cDnyilFU=';
-$channelSecret = '7c0f120975ee381f35f638bfb751bba6';
+$channelAccessToken = getenv('access_token');
+$channelSecret = getenv('channel_secret');
 
 $client = new LINEBotTiny($channelAccessToken, $channelSecret);
 foreach ($client->parseEvents() as $event) {
