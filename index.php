901874fa17fd507e3f8501f95aabd241ad385b6b
Chinatip Tangsiripat <chinatip@gmail.com>
2019-07-15 01:28:02 +0700

    fix

--- index.php
+++ index.php
@@ -7,13 +7,13 @@ $client->setApplicationName('LineBot PHP');
 $client->setScopes([\Google_Service_Sheets::SPREADSHEETS]);
 $client->setAccessType('offline');
 $client->setAuthConfig(__DIR__ . '/credentials.json');
-$service = new Google_Service_Sheets($client);/*
+$service = new Google_Service_Sheets($client);
 $spreadsheetId = '10HCCj0qKKf4OS0xzaBUrk2LdYozoZv3fOQe9Ar1cO1M';
 
 $range = 'Sheet1!:A1:A1';
 $response = $service->spreadsheets_values->get($spreadsheetId, $range);
 $value = $response->getValues();
-$row = $value[0];
+$row = $value[0];/*
 */
 
 require_once('./LINEBotTiny.php');
@@ -30,7 +30,7 @@ foreach ($client->parseEvents() as $event) {
             $message = $event['message'];
             switch ($message['type']) {
                 case 'text':
-                    $reText = $message['text'] . ' Test1';
+                    $reText = $message['text'] . ' Test1' . $row[0];
                     $client->replyMessage([
                         'replyToken' => $event['replyToken'],
                         'messages' => [
