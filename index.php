bec13375a93a4bf39225523e80bdbea12614812f
Chinatip Tangsiripat <chinatip@gmail.com>
2019-07-15 00:52:04 +0700

    fix

--- index.php
+++ index.php
@@ -1,20 +1,23 @@
 <?php
-/**
- * Copyright 2016 LINE Corporation
- *
- * LINE Corporation licenses this file to you under the Apache License,
- * version 2.0 (the "License"); you may not use this file except in compliance
- * with the License. You may obtain a copy of the License at:
- *
- *   https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations
- * under the License.
- */
+
+require __DIR__ . '/vendor/autoload.php';
+$client = new \Google_Client();
+$client->setApplicationName('LineBot PHP');
+$client->setScopes([\Google_Service_Sheets::SPREADSHEETS]);
+$client->setAccessType('offlice');
+$client->setAuthType(__DIR__ . '/credentials.json');
+$service = new Google_Service_Sheets($client);
+$spreadsheetId = '10HCCj0qKKf4OS0xzaBUrk2LdYozoZv3fOQe9Ar1cO1M';
+
+$range = 'Sheet1!:A1';
+$response = $service->spreadsheets_values->get($spreadsheetId, $range);
+$value = $response->getValues();
+$row = $value[0];
+
+
 require_once('./LINEBotTiny.php');
+
+
 
 $channelAccessToken = getenv('access_token');
 $channelSecret = getenv('channel_secret');
@@ -26,7 +29,7 @@ foreach ($client->parseEvents() as $event) {
             $message = $event['message'];
             switch ($message['type']) {
                 case 'text':
-                    $reText = $message['text'] . ' Test';
+                    $reText = $message['text'] . ' Test' . $row[0];
                     $client->replyMessage([
                         'replyToken' => $event['replyToken'],
                         'messages' => [
