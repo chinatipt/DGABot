39559f5301d737b81faa408d3b1abd49da249a1e
Chinatip Tangsiripat <chinatip@gmail.com>
2019-07-15 13:02:36 +0700

    test

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-/*
+
 require __DIR__ . '/vendor/autoload.php';
 $client = new \Google_Client();
 $client->setApplicationName('LineBot PHP');
@@ -28,13 +28,13 @@ $result = $service->spreadsheets_values->update(
     $body,
     $params
 );
-*/
-require_once('./BOT2Sheet.php');
+
+//require_once('./BOT2Sheet.php');
 require_once('./LINEBotTiny.php');
 
-$service = new BOT2Sheet();
-$value = $service.readRange('10HCCj0qKKf4OS0xzaBUrk2LdYozoZv3fOQe9Ar1cO1M','Sheet1!A1:B1');
-$row = $value[0];
+//$service = new BOT2Sheet();
+//$value = $service.readRange('10HCCj0qKKf4OS0xzaBUrk2LdYozoZv3fOQe9Ar1cO1M','Sheet1!A1:B1');
+//$row = $value[0];
 
 $channelAccessToken = getenv('access_token');
 $channelSecret = getenv('channel_secret');
