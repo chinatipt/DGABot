ddd7200fe84482d5c98ef774988757d31693642d
Chinatip Tangsiripat <chinatip@gmail.com>
2019-07-15 13:54:28 +0700

    change

--- index.php
+++ index.php
@@ -34,7 +34,7 @@ require_once('./LINEBotTiny.php');
 
 $service = new BOT2Sheet();
 $service->connect();
-$value = $service.readRange('10HCCj0qKKf4OS0xzaBUrk2LdYozoZv3fOQe9Ar1cO1M','Sheet1!A1:B1');
+$value = $service.readRange("10HCCj0qKKf4OS0xzaBUrk2LdYozoZv3fOQe9Ar1cO1M","Sheet1!A1:B1");
 $row = $value[0];
 
 $channelAccessToken = getenv('access_token');
