449bfb4ebaaedcc78b87b69d1bb97ccb00c2ede8
Chinatip Tangsiripat <chinatip@gmail.com>
2019-07-15 10:52:00 +0700

    kk

--- Bot2Sheet.php
+++ Bot2Sheet.php
@@ -1,19 +1,19 @@
 <?php
 
 
-
 class BOT2Sheet
 {
     private $service;
     public function __construct()
     {
-        
+        require __DIR__ . '/vendor/autoload.php';
         $client = new \Google_Client();
         $client->setApplicationName('LineBot PHP');
         $client->setScopes([\Google_Service_Sheets::SPREADSHEETS]);
         $client->setAccessType('offline');
         $client->setAuthConfig(__DIR__ . '/credentials.json');
-        $this->$service = new Google_Service_Sheets($client);
+        $service = new Google_Service_Sheets($client);
+        $this->$service = $service;
     }
 
     public function readSheet($spreadsheetId, $range)
