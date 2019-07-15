1ac39cf192a18103470832886d0a51cd86ac7f1d
Chinatip Tangsiripat <chinatip@gmail.com>
2019-07-15 13:42:50 +0700

    test

--- index.php
+++ index.php
@@ -34,8 +34,8 @@ require_once('./LINEBotTiny.php');
 
 $service = new BOT2Sheet();
 $service->connect();
-$value = $service.readRange('10HCCj0qKKf4OS0xzaBUrk2LdYozoZv3fOQe9Ar1cO1M','Sheet1!A1:B1');
-$row = $value[0];
+//$value = $service.readRange('10HCCj0qKKf4OS0xzaBUrk2LdYozoZv3fOQe9Ar1cO1M','Sheet1!A1:B1');
+//$row = $value[0];
 
 $channelAccessToken = getenv('access_token');
 $channelSecret = getenv('channel_secret');
