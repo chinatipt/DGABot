3e8f9018989da51a2fc31c4fe3143789a7a03f16
Chinatip Tangsiripat <chinatip@gmail.com>
2019-07-18 16:43:23 +0700

    fix

--- TestScrap.php
+++ TestScrap.php
@@ -1,5 +1,37 @@
 <?php
 
-    echo ("hello");
+    include "simple_html_dom.php";
+
+    $postFields = array(
+        "txtUserName" => "5890266",
+        "txtPassword" => "0s0MhuX5",
+        "btnLogIn" => "เข้าระบบ"
+    );
+
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, "https://intranet.rsu.ac.th/Signin.aspx");
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
+    curl_setopt($ch, CURLOPT_CAINFO, getcwd() . '\etc\cacert.pem');
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    //curl_setopt($ch, CURLOPT_POST, true);
+    //curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($postFields));
+    $response = curl_exec($ch);
+
+    if(curl_errno($ch)) // check for execution errors
+    {
+        echo 'Scraper error: ' . curl_error($ch);
+        exit;
+    }
+
+    curl_close($ch);
+
+    echo($response);
+    //$html = new simple_html_dom();
+    //$html->load($response);
+
+    //echo($html);
 
 ?>
--- index.php
+++ index.php
@@ -1,34 +1,5 @@
 <?php
 
-/*
-require __DIR__ . '/vendor/autoload.php';
-$client = new \Google_Client();
-$client->setApplicationName('LineBot PHP');
-$client->setScopes([\Google_Service_Sheets::SPREADSHEETS]);
-$client->setAccessType('offline');
-$client->setAuthConfig(__DIR__ . '/credentials.json');
-$service = new Google_Service_Sheets($client);
-$spreadsheetId = '10HCCj0qKKf4OS0xzaBUrk2LdYozoZv3fOQe9Ar1cO1M';
-
-$response = $service->spreadsheets_values->get($spreadsheetId, 'Sheet1!A1:B1');
-$value = $response->getValues();
-$row = $value[0];
-
-
-$values = [ ['Chin'] ];
-$body = new Google_Service_Sheets_ValueRange([
-    'values' => $values
-]);
-$params = [
-    'valueInputOption' => 'RAW'
-];
-$result = $service->spreadsheets_values->update(
-    $spreadsheetId,
-    'Sheet1!A2:A2',
-    $body,
-    $params
-);
-*/
 require_once('./BOT2Sheet.php');
 require_once('./LINEBotTiny.php');
 
