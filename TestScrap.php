c996ca64887511a7ea754f0be922ab3b6ac41703
Chinatip Tangsiripat <chinatip@gmail.com>
2019-07-19 13:13:35 +0700

    fix

--- TestScrap.php
+++ TestScrap.php
@@ -1,37 +1,16 @@
 <?php
 
-    include "simple_html_dom.php";
+    $ch = curl_init('https://script.google.com/macros/s/AKfycbxqpJIVwnCZz5YMx1MNpgPH1LBy45TapnY39I04shu6ON86EwSX/exec');
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-    $postFields = array(
-        "txtUserName" => "5890266",
-        "txtPassword" => "0s0MhuX5",
-        "btnLogIn" => "เข้าระบบ"
-    );
-
-
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, "https://intranet.rsu.ac.th/Signin.aspx");
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 1);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
-    curl_setopt($ch, CURLOPT_CAINFO, __DIR__ . "/etc/cer.crt");
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    //curl_setopt($ch, CURLOPT_POST, true);
-    //curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($postFields));
-    $response = curl_exec($ch);
-
-    if(curl_errno($ch)) // check for execution errors
-    {
-        echo 'Scraper error: ' . curl_error($ch);
-        exit;
-    }
+    $page = curl_exec($ch);
 
     curl_close($ch);
 
-    echo($response);
-    //$html = new simple_html_dom();
-    //$html->load($response);
+    $grade = json_decode($page);
 
-    //echo($html);
+    echo($grade[0][1]);
 
 ?>
--- index.php
+++ index.php
@@ -3,11 +3,26 @@
 require_once('./BOT2Sheet.php');
 require_once('./LINEBotTiny.php');
 
+/* Read Google Sheet
 $service = new BOT2Sheet(__DIR__);
 $value = $service->readRange('10HCCj0qKKf4OS0xzaBUrk2LdYozoZv3fOQe9Ar1cO1M','Sheet1!A1:B1');
 $row = $value[0];
 
 $service->writeRange('10HCCj0qKKf4OS0xzaBUrk2LdYozoZv3fOQe9Ar1cO1M','Sheet1!A3:B3', [ ['aaa','bbb'], ]);
+*/
+
+$ch = curl_init('https://script.google.com/macros/s/AKfycbxqpJIVwnCZz5YMx1MNpgPH1LBy45TapnY39I04shu6ON86EwSX/exec');
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+$page = curl_exec($ch);
+
+curl_close($ch);
+
+$grade = json_decode($page);
+
+//echo($grade[0][1]);
 
 $channelAccessToken = getenv('access_token');
 $channelSecret = getenv('channel_secret');
@@ -24,7 +39,7 @@ foreach ($client->parseEvents() as $event) {
                         'messages' => [
                             [
                                 'type' => 'text',
-                                'text' => $message['text'] . $row[0]
+                                'text' => $message['text'] . $grade[0][1]
                             ]
                         ]
                     ]);
